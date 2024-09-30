944757ed934eff5be256b135015abc74b190ab85
camilarochatec <camilarocha.tec@gmail.com>
2024-09-30 17:32:47 -0300

    resolvendo atividade js

---
+++ README.md
@@ -0,0 +1,75 @@
+Atividades de lógica com estruturas condicionais
+
+
+Problema: Verifique se um número é positivo, negativo ou zero.
+Descrição: Escreva um programa que solicite ao usuário um número e, em seguida, informe se o número é positivo, negativo ou zero.
+
+
+
+
+Problema: Classificar um triângulo.
+Descrição: Crie um programa que peça ao usuário para inserir os comprimentos dos três lados de um triângulo e determine se é equilátero, isósceles ou escaleno.
+
+
+
+
+Problema: Verificar se um ano é bissexto.
+Descrição: Escreva um programa que solicite ao usuário um ano e informe se ele é bissexto ou não.
+
+
+
+
+Problema: Determinar o maior entre três números.
+Descrição: Desenvolva um programa que peça três números ao usuário e determine qual é o maior deles.
+
+
+
+
+Problema: Verificar se um número é par ou ímpar.
+Descrição: Crie um programa que solicite um número ao usuário e informe se é par ou ímpar.
+
+
+
+
+Problema: Calcular o preço final de um produto com base na quantidade comprada.
+Descrição: Escreva um programa que solicite ao usuário o preço de um produto e a quantidade comprada. Se a quantidade comprada for maior que 10, aplique um desconto de 10%. Caso contrário, não aplique desconto.
+
+
+
+
+Problema: Converter uma nota em conceito.
+Descrição: Crie um programa que peça ao usuário para inserir uma nota de 0 a 100 e, em seguida, atribua um conceito (A entre 100 e 80, B entre 79 e 60, C entre 59 e 40, D entre 39 e 20 ou F abaixo de 20) com base na faixa de notas.
+
+
+
+
+Problema: Verificar se uma letra é vogal ou consoante.
+Descrição: Desenvolva um programa que solicite ao usuário uma letra e informe se é uma vogal ou consoante.
+
+
+
+
+Problema: Determinar o dia da semana com base em um número.
+Descrição: Escreva um programa que solicite ao usuário um número de 1 a 7 e informe o dia correspondente da semana (por exemplo, 1 para segunda-feira, 2 para terça-feira, etc.).
+
+
+
+
+Problema: Calcular o imposto de renda de uma pessoa com base em sua renda anual.
+Descrição: Crie um programa que peça à pessoa sua renda anual e, em seguida, calcule o imposto de renda de acordo com a tabela de alíquotas do imposto de renda.
+
+
+Tabela do imposto de renda
+De R$ 24.511,93 até R$ 33.919,80
+7,5%
+R$ 1.838,39
+De R$ 33.919,81 até R$ 45.012,60
+15,0%
+R$ 4.382,38
+De R$ 45.012,61 até R$ 55.976,16
+22,5%
+R$ 7.758,32
+Acima de R$ 55.976,16
+27,5%
+R$ 10.557,13
+
---
+++ index.js
@@ -0,0 +1,109 @@
+//EXEMPLO1: PARA SABER SE É POSITIVO OU NEGATIVO OU 0
+
+// let numero = prompt('Digite um número');
+
+
+// if (numero > 0) {
+//     document.write('o número é positivo');
+// } else if (numero < 0) document.write('o número é negativo');
+// else {
+//     document.write('o número é 0');
+// }
+
+//EXEMPLO2: COMPRIMENTO DE UM TRIANGULO
+
+// const ladoA = prompt('Digite o comprimento do lado A: ');
+// const ladoB = prompt('Digite o comprimento do lado B: ');
+// const ladoC = prompt('Digite o comprimento do lado C: ');
+
+// if(ladoA ===  ladoB && ladoB === ladoC){
+// document.write('o triângulo é equilátero');
+// } else if (ladoA != ladoB && ladoB != ladoC) {
+//     document.write('é um triângulo escaleno');
+// } else {
+//     document.write('é um triângulo isósceles');
+// }
+
+//EXEMPLO3: Verificar se um ano é bissexto. (Um ano é bissexto se for divisível por 4.
+//Não é bissexto se for divisível por 100, a menos que também seja divisível por 400.)
+
+// function bissexto(ano) {
+//     return (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0);
+// }
+// let ano = prompt('Digite um ano');
+
+// if (bissexto (ano)){
+// document.write('o ano ' + ano + ' é bissexto.')
+// } else {
+//     document.write('o ano ' + ano + ' não é bissexto')
+// }
+
+//EXEMPLO4: Determinar o maior entre três números. (dificil)
+
+// let numero1 = prompt('Digite um número');
+// let numero2 = prompt('Digite outro número');
+// let numero3 = prompt('Digite mais um número');
+
+// if (numero1 > numero2 && numero3){
+//     document.write('o ' + numero1 + ' é o maior');
+
+// }else if(numero2 > numero1 && numero3){
+//     document.write('o ' + numero2 + ' é o maior');
+
+// }else{
+//     document.write('o ' + numero3 + ' é o maior');
+// }
+
+//EXEMPLO5: Verificar se um número é par ou ímpar.
+
+// let numero = prompt('Digite um número');
+
+// if (numero % 2 == 0) {
+//     document.write('É par');
+// } else {
+//     document.write('É Ímpar')
+// }
+
+//EXEMPLO6: Calcular o preço final de um produto com base na quantidade comprada. (DIFÍCIL)
+
+//EXEMPLO7: Converter uma nota em conceito. (DIFÍCIL)
+
+// let nota = prompt("Digite uma nota de 0 a 100:")
+// function determinarConceito(nota) {
+//     let conceito
+// };
+// if (nota >= 80 && nota <= 100) {
+//     conceito = 'A';
+//     document.write('A')
+// } else if (nota >= 60 && nota < 80) {
+//     conceito = 'B';
+//     document.write('B')
+// } else if (nota >= 40 && nota < 60) {
+//     conceito = 'C';
+//     document.write('C')
+// } else if (nota >= 20 && nota < 40) {
+//     conceito = 'D';
+//     document.write('D')
+// } else if (nota >= 0 && nota < 20) {
+//     conceito = 'F';
+//     document.write('F')
+// }
+
+//EXEMPLO8: Verificar se uma letra é vogal ou consoante. (DIFICIL)
+
+// let letra = prompt("Digite uma letra:");
+// if (letra = 'a' || 'e' ||'i'|| 'o' ||'u') {
+//     document.write('A letra ' + letra + ' é uma vogal.');
+// } else {
+//     document.write('A letra ' + letra + ' é uma consoante.');
+// }
+
+//EXEMPLO9: Determinar o dia da semana com base em um número. (DIFICIL)
+
+//EXEMPLO10: Calcular o imposto de renda de uma pessoa com base em sua renda anual. (DIFÍCIL)
+
+
+
+
+
+
